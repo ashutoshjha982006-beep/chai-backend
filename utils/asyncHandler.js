bfb650aed86745ab4aa16db9154173e578512c5d
ashutoshjha982006-beep <ashutoshjha982006@gmail.com>
2026-09-14 05:31:54 +0000

    response&errorhandling

--- utils/asyncHandler.js
+++ utils/asyncHandler.js
@@ -1,6 +1,14 @@
-
+const asyncHandler=(requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
+}
 
 export {asyncHandler}
+
+//const asyncHandler=() => {}
+//const asyncHandler=(func)=>()=>{}
+//const asyncHandler=(func)=>async()=>{}
 
 // const asyncHandler=(fn) => async (req,res,next) => {
 //     try{
@@ -12,3 +20,7 @@ export {asyncHandler}
 //         })
 //     }
 // }//higher order function fn ko as input accept karega
+
+
+
+//middle wares checking ke liye lagate hai
