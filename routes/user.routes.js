d0dae32c809edbc9f10002456aa4fe6b996bd4d2
ashutosh <ashutoshjha982006@gmail.com>
2026-09-19 16:03:07 +0530

    f

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,7 +4,20 @@ import { upload } from "../middlewares/multer.middleware.js"
 
 const router = Router()
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields([
+        {
+            name:"avatar",
+            maxCount:1
+        },
+        {
+            name:"coverImage",
+            maxCount:1
+        }
+    ])
+    ,
+    registerUser
+)
 /// http://localhost:8000/api/v1/users/login
 
 
